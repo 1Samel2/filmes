0d116b7da96e98553d04cb8c571c3e8673590db7
samuel <samuelsguedes@hotmail.com>
2023-07-12 00:01:22 -0300

    First commit

---
+++ src/services/api.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "f41859a04595a109d24417c3255ba418",
+    language: "pt-BR",
+    page: 1,
+  },
+});
